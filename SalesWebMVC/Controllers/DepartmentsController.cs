055698e268c87b6bbce49653a77eecc10b2c394f
Rafael Silva Holanda <rafaelsilvaholanda@edu.unifor.br>
2022-08-04 09:34:14 -0300

    Adicionado o controlador/model departament e a view index

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers {
+    public class DepartmentsController : Controller {
+        public IActionResult Index() {
+            var departaments = new List<Departament>();
+            departaments.Add(new Departament(1, "Eletronics"));
+            departaments.Add(new Departament(2, "Fashion"));
+            return View(departaments);
+        }
+    }
+}
---
+++ SalesWebMVC/Models/Departament.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models {
+    public class Departament {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Departament(int id, string name) {
+            Id = id;
+            Name = name;
+        }
+
+    }
+}
